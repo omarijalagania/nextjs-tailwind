88354a301083ab96b2ef17b2ab3b992b4e65d66d
omari <omari.jalagania@gmail.com>
2021-11-06 20:53:29 +0400

    modal component

---
+++ components/Modal.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Modal() {
+  return (
+    <div className="border-2 ">
+      <h3>Select Timezone</h3>
+      <p>Please select your timezone</p>
+      <select>
+        <option>Asia/Tbilisi(09:30)</option>
+      </select>
+      <button>Set my Timezone</button>
+      <h3>Cancel</h3>
+    </div>
+  );
+}
+
+export default Modal;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 
+import Modal from "../components/Modal";
+
 export default function Home() {
   return (
     <div className="text-center">
       <p className="text-red-400 font-bold my-4">Tailwind With NextJs</p>
+      <Modal />
     </div>
   );
 }
