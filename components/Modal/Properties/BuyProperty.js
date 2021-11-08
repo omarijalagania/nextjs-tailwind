17a347cef561eda9bd5f82a05b792cb47c94fa65
Irakli <koregm@gmail.com>
2021-11-08 14:32:19 +0400

    Add buy property

---
+++ components/Modal/Properties/BuyProperty.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const BuyProperty = () => {
+  return (
+    <div class="max-w-screen-lg bg-white shadow-2xl rounded-lg mx-auto text-center py-12 mt-4 rounded border-l-4 border-gray-500 flex justify-between">
+      <div className="ml-10">
+        <div>Buy Property</div>
+        <span>1 hr.</span>
+      </div>
+
+      <div class=" flex justify-end">
+        <div class="inline-flex justify-center rounded-full bg-black shadow w-48 mr-5">
+          <a href="#" class="text-green-300 font-bold mt-3">
+            Select
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BuyProperty;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 
 import Modal from "../components/Modal/Modal";
+import BuyProperty from "../components/Modal/Properties/BuyProperty";
 
 export default function Home() {
   return (
     <div className="text-center">
       <p className="text-red-400 font-bold my-4">Tailwind With NextJs</p>
       <Modal />
+      <BuyProperty/>
     </div>
   );
 }
