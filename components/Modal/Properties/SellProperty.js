98cf447d6dd1bbc04d9c20694e7b143fe766e57d
Irakli <koregm@gmail.com>
2021-11-08 17:34:35 +0400

    Add hourglass icon on properties

--- components/Modal/Properties/SellProperty.js
+++ components/Modal/Properties/SellProperty.js
@@ -2,14 +2,19 @@ import React from "react";
 
 const SellProperty = () => {
   return (
-    <div class="max-w-screen-lg bg-white shadow-2xl rounded-lg mx-auto text-center py-12 mt-4 rounded border-l-4 border-black flex justify-between">
+    <div class="max-w-screen-lg bg-white shadow-2xl rounded-lg mx-auto text-center py-8 mt-4 rounded border-l-4 border-black flex justify-between">
       <div className="ml-10">
-        <div>Sell Property</div>
-        <span>1 hr.</span>
+        <div className="font-bold">Sell Property</div>
+        <div className="flex pt-2">
+          <img
+            className="w-6 h-6"
+            src="https://img.icons8.com/material-outlined/24/000000/hourglass--v1.png"
+          />
+          <span>1 hr.</span>
+        </div>
       </div>
-
       <div class=" flex justify-end">
-        <div class="inline-flex justify-center rounded-full bg-black shadow w-48 mr-5">
+        <div class="inline-flex justify-center rounded-full bg-black shadow w-48 h-12 mr-5">
           <a href="#" class="text-green-300 font-bold mt-3">
             Select
           </a>
