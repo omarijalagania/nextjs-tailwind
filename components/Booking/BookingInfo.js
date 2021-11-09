1e02e4a6feef38ec696823aad0ee021f8cdcdffd
omari <omari.jalagania@gmail.com>
2021-11-09 20:22:55 +0400

    added icons and border radius

--- components/Booking/BookingInfo.js
+++ components/Booking/BookingInfo.js
@@ -3,10 +3,25 @@ import React from "react";
 function BookingInfo() {
   return (
     <div className="w-6/12 m-7 shadow  overflow-hidden">
-      <div className="bg-blue-400 rounded-md p-4">
+      <div className="bg-blue-400 rounded-t-md p-4">
         <div className="flex justify-between px-4 h-20">
           <h3 className="text-white font-bold">Booking Info</h3>
-          <button className="h-6 text-white">x</button>
+          <button className="h-6 text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </div>
         <ul className="flex">
           <li className="mr-3">
@@ -21,31 +36,172 @@ function BookingInfo() {
       </div>
       <div className="flex">
         <div>
-          <div className="bg-blue-400 rounded-md mt-4 w-64 h-32 relative">
-            <p className="text-white font-bold text-left px-3">11-04-2021</p>
+          <div className="bg-blue-400 rounded-r-md mt-4 w-64 h-32 relative">
+            <div className="flex p-3 text-white ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                />
+              </svg>
+              <p className="text-white font-bold text-left px-3">11-04-2021</p>
+            </div>
+
             <p className="text-left text-white px-3">11:00AM - 12:00AM</p>
-            <div className="text-white text-left px-3 absolute bottom-3">
+
+            <div className="text-white text-left px-3 absolute bottom-3 flex">
+              <div className="pr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
               <p>1 hr.</p>
             </div>
           </div>
-          <div className="shadow p-4 bg-gray-200 w-64 mt-2">
+          <div className="shadow rounded-r-md p-4 bg-gray-200 w-64 mt-2">
             <p>Obz234dfe</p>
           </div>
           <div className="text-left p-3">
-            <p className="text-gray-700 font-light">Created by client</p>
-            <small>11-03-2021 03:34AM</small>
+            <div className="flex text-blue-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                />
+              </svg>
+              <p className="text-gray-700 font-light">Created by client</p>
+            </div>
+
+            <small className="p-4">11-03-2021 03:34AM</small>
           </div>
         </div>
         {/* Other side */}
         <div className="p-5">
-          <h3 className="font-bold text-left">Some Info</h3>
-          <p className="text-gray-700 font-light">
-            Client timezone: America/Los Angeles
-          </p>
-          <p className="text-gray-700 font-light text-left">Test</p>
-          <p className="text-gray-700 font-light text-left">St. Paul's</p>
-          <p className="text-gray-700 font-light text-left">Some address bar</p>
-          <p className="text-gray-700 font-light text-left">0.00$</p>
+          <h3 className="font-bold text-left py-2">Some Info</h3>
+          <div className="flex text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+              />
+            </svg>
+            <p className="text-gray-700 font-light pl-2 mb-2">
+              Client timezone: America/Los Angeles
+            </p>
+          </div>
+          <div className="flex text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+              />
+            </svg>
+            <p className="text-gray-700 font-light text-left pl-2 mb-2">Test</p>
+          </div>
+          <div className="flex text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+            <p className="text-gray-700 font-light text-left pl-2 mb-2">
+              St. Paul's
+            </p>
+          </div>
+          <div className="flex text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+              />
+            </svg>
+            <p className="text-gray-700 font-light text-left pl-2 mb-2">
+              Some address bar
+            </p>
+          </div>
+          <div className="flex text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
+              />
+            </svg>
+            <p className="text-gray-700 font-light text-left pl-2">0.00$</p>
+          </div>
         </div>
       </div>
     </div>
